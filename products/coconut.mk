069fe4786bcad85e6429258cfbc8d428f3232347
Krzysztof B <pikpoczek12@gmail.com>
2012-06-12 15:51:49 +0200

    added coconut (WT19i)

---
+++ products/coconut.mk
@@ -0,0 +1,35 @@
+# Inherit AOSP device configuration for smultron
+$(call inherit-product, device/semc/smultron/device_coconut.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Call KXP vendor to get unofficial additions
+$(call inherit-product, vendor/kxp/common_kxp.mk)
+
+# coconut overlay
+#PRODUCT_PACKAGE_OVERLAYS += vendor/kxp/overlay/coconut
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_coconut
+PRODUCT_BRAND := SEMC
+PRODUCT_DEVICE := coconut
+PRODUCT_MODEL := Live with Walkman
+PRODUCT_MANUFACTURER := SEMC
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST15i BUILD_FINGERPRINT="SEMC/LT18i_0000-0000/LT18i:4.0.3/4.1.C.0.7/-H9_3w:user/release-keys" PRIVATE_BUILD_DESC="WT19i-user 4.0.3 4.1.C.0.7 -H9_3w test-keys"
+
+
+# Camera
+PRODUCT_PACKAGES += \
+    Camera \
+    Mms \
+    Music
+
+# Copy coconut specific prebuilt files
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_320_480.zip:system/media/bootanimation.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,3 +7,5 @@ add_lunch_combo aokp_es209ra-userdebug
 add_lunch_combo aokp_shakira-userdebug
 add_lunch_combo aokp_smultron-userdebug
 add_lunch_combo aokp_satsuma-userdebug
+add_lunch_combo aokp_coconut-userdebug
+
