744322a7a796366e2f6bd7ce434c325f9d09a7b1
stelios97 <stilianos1000@gmail.com>
2012-06-18 21:17:51 +0300

    Added mimmi and robyn

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,8 @@ add_lunch_combo aokp_urushi-userdebug
 add_lunch_combo aokp_zeus-userdebug
 add_lunch_combo aokp_es209ra-userdebug
 add_lunch_combo aokp_shakira-userdebug
+add_lunch_combo aokp_robyn-userdebug
+add_lunch_combo aokp_mimmi-userdebug
 add_lunch_combo aokp_smultron-userdebug
 add_lunch_combo aokp_satsuma-userdebug
 add_lunch_combo aokp_coconut-userdebug
