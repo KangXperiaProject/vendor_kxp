b0f6de13c42f9a22a75896908af2802199471c37
Arnav Gupta <championswimmer@gmail.com>
2012-06-21 11:28:00 +0530

    Revert "terminal too"
    This reverts commit b2fec1da238eae3204c5b155a2ccffe4fdf947aa.

--- common_kxp.mk
+++ common_kxp.mk
@@ -2,7 +2,7 @@
 #PRODUCT_COPY_FILES += \
 #   vendor/kxp/prebuilt/bootlogo/$(PRODUCT_DEVICE).rle:root/initlogo.rle
 
-#Use a free file manager and android terminal untill AOKP adds alternative opensource packages
+#Use a free file manager untill AOKP adds an official filemanager
 PRODUCT_COPY_FILES += \
     vendor/kxp/prebuilt/app/FileManager.apk:system/app/FileManager.apk \
     vendor/kxp/prebuilt/app/AndroidTerm.apk:system/app/AndroidTerm.apk \
---
+++ products/mimmi.mk
@@ -0,0 +1,31 @@
+# Inherit device configuration
+$(call inherit-product, device/semc/mimmi/device_mimmi.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Call kxp vendor to get unofficial additions
+$(call inherit-product, vendor/kxp/common_kxp.mk)
+
+# mimmi overlay
+#PRODUCT_PACKAGE_OVERLAYS += vendor/kxp/overlay/mimmi
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_mimmi
+PRODUCT_BRAND := SEMC
+PRODUCT_DEVICE := mimmi
+PRODUCT_MODEL := U20i
+PRODUCT_MANUFACTURER := Sony Ericsson
+
+# Camera
+PRODUCT_PACKAGES += \
+    Camera \
+    Mms \
+    Music
+
+# Copy anzu specific prebuilt files
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_240_320.zip:system/media/bootanimation.zip
