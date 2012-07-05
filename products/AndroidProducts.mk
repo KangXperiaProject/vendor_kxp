373e89be05376838a5bbabd6ec88d3275dd5dcd6
stelios97 <stilianos1000@gmail.com>
2012-07-05 14:48:39 +0300

    Update ics

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -7,4 +7,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/smultron.mk \
     $(LOCAL_DIR)/satsuma.mk \
     $(LOCAL_DIR)/coconut.mk \
+    $(LOCAL_DIR)/robyn.mk \
+    $(LOCAL_DIR)/shakira.mk \
+    $(LOCAL_DIR)/mimmi.mk \
     $(LOCAL_DIR)/mango.mk 
