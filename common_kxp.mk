e6af9d57a741132e0e8a63b9e8a4660c1032fa41
Arnav Gupta <championswimmer@gmail.com>
2012-09-08 13:49:49 +0530

    will populate unoficial kxp makefile later... for now keep it commented
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- common_kxp.mk
+++ common_kxp.mk
@@ -3,19 +3,19 @@
 #   vendor/kxp/prebuilt/bootlogo/$(PRODUCT_DEVICE).rle:root/initlogo.rle
 
 #Use a free file manager untill AOKP adds an official filemanager
-PRODUCT_COPY_FILES += \
-    vendor/kxp/prebuilt/app/FileManager.apk:system/app/FileManager.apk \
-    vendor/kxp/prebuilt/app/AndroidTerm.apk:system/app/AndroidTerm.apk \
-    vendor/kxp/prebuilt/lib/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+#PRODUCT_COPY_FILES += \
+#    vendor/kxp/prebuilt/app/FileManager.apk:system/app/FileManager.apk \
+#    vendor/kxp/prebuilt/app/AndroidTerm.apk:system/app/AndroidTerm.apk \
+#    vendor/kxp/prebuilt/lib/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
 
 # Unofficial packages that KangXperiaProject uses from trees outside AOKP
-PRODUCT_PACKAGES += \
-    Apollo \
-    Spearmint \
-    Grape \
-    Bubble \
-    Cherry \
-    Orange \
-    DSPManager \
-    Trebuchet \
-    AchepClock
+#PRODUCT_PACKAGES += \
+#    Apollo \
+#    Spearmint \
+#    Grape \
+#    Bubble \
+#    Cherry \
+#    Orange \
+#    DSPManager \
+#    Trebuchet \
+#    AchepClock
