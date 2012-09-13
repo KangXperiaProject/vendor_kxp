d4ce0b996a0fe97ae931958517b7426427db9e70
Arnav Gupta <championswimmer@gmail.com>
2012-09-13 14:10:36 +0530

    3rd party apps : add terminal emulator (Jack Pal), FIle Manager (RhythmSoft)
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- common_kxp.mk
+++ common_kxp.mk
@@ -5,11 +5,13 @@ USER := kxp_build1-r3
 #PRODUCT_COPY_FILES += \
 #   vendor/kxp/prebuilt/bootlogo/$(PRODUCT_DEVICE).rle:root/initlogo.rle
 
-#Use a free file manager untill AOKP adds an official filemanager
-#PRODUCT_COPY_FILES += \
-#    vendor/kxp/prebuilt/app/FileManager.apk:system/app/FileManager.apk \
-#    vendor/kxp/prebuilt/app/AndroidTerm.apk:system/app/AndroidTerm.apk \
-#    vendor/kxp/prebuilt/lib/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+#Use a free file manager and terminal untill AOKP adds an official filemanager
+PRODUCT_COPY_FILES += \
+    vendor/kxp/prebuilt/app/AndroidTerm.apk:system/app/AndroidTerminalEmulator.apk \
+    vendor/kxp/prebuilt/lib/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
+    vendor/kxp/prebuilt/app/FileManager.apk:system/app/FileManager.apk
+
+
 
 # Unofficial packages that KangXperiaProject uses from trees outside AOKP
 PRODUCT_PACKAGES += \
