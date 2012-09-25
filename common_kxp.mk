bb81f9e567d42600bb1d4e3c2cb29dcef97916f5
Arnav Gupta <championswimmer@gmail.com>
2012-09-26 00:58:56 +0530

    bump buildver
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- common_kxp.mk
+++ common_kxp.mk
@@ -1,18 +1,18 @@
 #we will change the $USER variable here to make our builds have better naming :)
-USER := kxp_build2-r2
+USER := kxp_build3
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=kxp \
     ro.goo.rom=kxp \
-    ro.goo.version=4110202
+    ro.goo.version=4110300
 #goo version is like this -> 411 for jellybean 4.1.1, 02 for build2, 02 for r2
 
 #Ota Updater Pro app
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=$(PRODUCT_DEVICE)_$(USER) \
     otaupdater.otaver=$(USER) \
-    otaupdater.otatime=20120925-0000 \
+    otaupdater.otatime=20120927-0000 \
     otaupdater.noflash=1 \
     otaudpater.rebootcmd=NULL
 
