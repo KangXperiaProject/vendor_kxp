ac1441f37c5b6e7c5647706e20e46399cbc066b8
Arnav Gupta <championswimmer@gmail.com>
2012-10-01 00:43:41 +0530

    bump buildver add legacycamera
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- common_kxp.mk
+++ common_kxp.mk
@@ -1,18 +1,18 @@
 #we will change the $USER variable here to make our builds have better naming :)
-USER := kxp_build3
+USER := kxp_build3-r2
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=kxp \
     ro.goo.rom=kxp \
-    ro.goo.version=4110300
+    ro.goo.version=4110302
 #goo version is like this -> 411 for jellybean 4.1.1, 02 for build2, 02 for r2
 
 #Ota Updater Pro app
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=$(PRODUCT_DEVICE)_$(USER) \
     otaupdater.otaver=$(USER) \
-    otaupdater.otatime=20120927-0000 \
+    otaupdater.otatime=20120930-0000 \
     otaupdater.noflash=1 \
     otaudpater.rebootcmd=NULL
 
@@ -35,7 +35,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Apollo \
     DSPManager \
-    Trebuchet 
+    Trebuchet \
+    LegacyCamera 
 #    Spearmint \
 #    Grape \
 #    Bubble \
