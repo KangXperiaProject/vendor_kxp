9498ae5f4fa447f0b69d621a6254f57095ff0f54
Arnav Gupta <championswimmer@gmail.com>
2012-09-30 20:55:29 -0700

    we have build4 now
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- common_kxp.mk
+++ common_kxp.mk
@@ -1,18 +1,18 @@
 #we will change the $USER variable here to make our builds have better naming :)
-USER := kxp_build3-r2
+USER := kxp_build4
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=kxp \
     ro.goo.rom=kxp \
-    ro.goo.version=4110302
+    ro.goo.version=4110400
 #goo version is like this -> 411 for jellybean 4.1.1, 02 for build2, 02 for r2
 
 #Ota Updater Pro app
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=$(PRODUCT_DEVICE)_$(USER) \
     otaupdater.otaver=$(USER) \
-    otaupdater.otatime=20120930-0000 \
+    otaupdater.otatime=20121003-0000 \
     otaupdater.noflash=1 \
     otaudpater.rebootcmd=NULL
 
