195ad3c315acc17133c51ba3b77671bb8c7c2d94
Krzysztof B <pikpoczek12@gmail.com>
2012-10-02 18:06:15 +0200

    Updated OTA Updater app to 1.0.4 Signed-off-by: Krzysztof B <pikpoczek12@gmail.com>

--- common_kxp.mk
+++ common_kxp.mk
@@ -17,7 +17,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     otaudpater.rebootcmd=NULL
 
 PRODUCT_COPY_FILES += \
-    vendor/kxp/prebuilt/app/OTAUpdater-1.0.3.apk:system/app/OTAUpdater-1.0.3.apk
+    vendor/kxp/prebuilt/app/OTAUpdater-1.0.4.apk:system/app/OTAUpdater-1.0.4.apk
 
 #we will use our own bootlogo 
 PRODUCT_COPY_FILES += \
