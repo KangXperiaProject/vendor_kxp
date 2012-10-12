add6811a4b240e79cd54ef2923276a4a0d0b0e71
Krzysztof B <pikpoczek12@gmail.com>
2012-10-12 19:22:12 +0200

    OTA Updater 1.0.5 patch2 - didn't added other changes, god why? Signed-off-by: Krzysztof B <pikpoczek12@gmail.com> Change-Id: I4352f9509f538b2fb7106f2df5402cdf80a3340f

--- common_kxp.mk
+++ common_kxp.mk
@@ -17,7 +17,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     otaudpater.rebootcmd=NULL
 
 PRODUCT_COPY_FILES += \
-    vendor/kxp/prebuilt/app/OTAUpdater-1.0.4.apk:system/app/OTAUpdater-1.0.4.apk
+    vendor/kxp/prebuilt/app/OTAUpdater-1.0.5.apk:system/app/OTAUpdater-1.0.5.apk
 
 #we will use our own bootlogo 
 PRODUCT_COPY_FILES += \
